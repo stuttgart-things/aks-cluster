b4c34aa16ffb2ae03eed5cb5d1342ed9ce3c1d60
anacalva <132065767+anacalva@users.noreply.github.com>
2023-05-16 14:27:31 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,42 +1,19 @@
-variable "client_id" {
-  description = "Azure Kubernetes Service Cluster service principal"
-  default = ""
+ variable "client_id" {
+  default     = false
+  description = ""
 }
 
 variable "client_secret" {
-  description = "Azure Kubernetes Service Cluster password"
-  default = ""
-}
-
-variable "resource_group_name" {
-  description = "Resource Group name"
-  default = "rg_aks"
-}
-variable "location" {
+  default     = false
   description = ""
-  default = "germanywestcentral"
 }
 
-variable "environment" {
+variable "tenant_id" {
+  default     =  false
   description = ""
-  default = "Demo"
 }
 
-variable "aks_name" {
-  description = "Azure Kubernetes Service Cluster name"
-}
-
-variable "node_count" {
-  description = "Azure Kubernetes Service Cluster node count"
-  default = 2
-}
-
-variable "vm_size" {
-  description = "Azure Kubernetes Service Cluster virtual machine size"
-  default = "Standard_B2s"
-}
-
-variable "os_disk_size_gb" {
-  description = "Azure Kubernetes Service Cluster OS size in GB"
-  default = 30
+variable "subscription_id" {
+  default     = false
+  description = ""
 }
